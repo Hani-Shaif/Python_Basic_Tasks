20057ea5f3b690cf7348b326b6f8344981c08d6f
Hani-Shaif <mohsenhani1991@gmail.com>
2023-06-17 21:50:42 +0200

    Add Operators Task

---
+++ Python_Task_Operators.py
@@ -0,0 +1,113 @@
+#===========================================
+#========= Python Basics Tasks =============
+#====== Python Basics Tasks Operators ======
+#===========================================
+
+# 1 - What is the differences between = , ==
+print("= اسناد قيمة الطرق الايمن للطرف الايسر")
+print("== ان قيمة الطرق الايسر تساوي القيمة التي بالطرف الايمن")
+
+# 2 - What do we mean by using !=
+print("Not Equal")
+
+# 3 - What is the operator precedence
+print("اولوية العمليات الحسابية")
+
+# 4 - Create a variable x with value of 10
+x = 10
+print(x)
+
+# 5 - Increase x value by 15 using assignment operators
+x = 20
+x += 15 # x = x + 15
+print(x)
+
+# 6 - Divide the x value by 5 using assignment operators
+x = 25
+x /= 5  # x = x / 5
+print(x)
+
+# 7 - Multiply the x value by 10 using assignment operator
+x = 20
+x *= 10   # x = x * 10
+print(x)
+
+# 8 - Get module of x on 3 using assignment operators
+x = 14
+x %= 3  # x = x % 3
+print(x)
+
+# 9 - Using python print the module of 11 to 4
+x = 18 % 4
+print(x)
+
+# 10 - Print the exponent of 2,3
+x = 2 ** 3
+print(x)
+
+# 11 - Divide 11 by 3 using python division
+x = 11 / 3
+print(x)
+
+# 12 - Can we divide 11 by 3 and get an integer number ?
+x = 11 / 3 # No => 11 / 3 = 3.66 (float)
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
