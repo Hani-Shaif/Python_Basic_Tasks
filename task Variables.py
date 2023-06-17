ec735617af28cffff66f8782fab6d819c90e5ef9
Hani-Shaif <mohsenhani1991@gmail.com>
2023-06-17 21:45:12 +0200

    AddVariables Task

---
+++ task Variables.py
@@ -0,0 +1,175 @@
+#===========================================
+#========= Python Basics Tasks =============
+#====== Python Basics Tasks Variables ======
+#===========================================
+
+# 1. Create a Boolean variable named x
+x = True
+print(x)
+print("----------------------------")
+
+# 2. Create an integer variable named y
+y = 12
+print(y)
+print("----------------------------")
+# 3. Create a float variable named z
+z = 2.9
+print(z)
+print("----------------------------")
+# 4. Create a string variable names s
+s = "Mystro"
+print(s)
+print("----------------------------")
+# 5. Convert the int variable to float
+x = 12
+y = float(x)
+print(y)
+print("----------------------------")
+# 6. Can we convert the str to int ?
+s = "100"
+x = str(s)
+print(x)
+print("----------------------------")
+# 7. Create a list of numbers from 1 to 5
+myList = [1,2,3,4,5]
+print(myList)
+print("----------------------------")
+#8. Create a tuple from 10 to 15
+myTuple = (10,11,12,13,14,15)
+print(myTuple)
+print("----------------------------")
+#9. Convert the list to tuple
+myList = [1,2,3,4,5]
+convertToTuple = tuple(myList)
+print(convertToTuple)
+print(type(convertToTuple))
+print("----------------------------")
+
+
+# 10. Create a dict of 3 values
+myDict = {1:"Hani",2:"Sami",3:"Ali"}
+print(myDict)
+print("----------------------------")
+
+# 11. Can we use semi colon ; with python
+print('Hi'); print('Hani'); print('How ware you?')
+#تشير الفاصلة المنقوطة في بايثون إلى الفصل وليس الإنهاء
+
+# 12. Python is interpreted or compiled ?
+
+
+# 13. What is the differences between low level & high level
+
+#===========================================
+#====== Python Basics Tasks Operators ======
+#===========================================
+
+# 1 - What is the differences between = , ==
+print("= اسناد قيمة الطرق الايمن للطرف الايسر")
+print("== ان قيمة الطرق الايسر تساوي القيمة التي بالطرف الايمن")
+
+# 2 - What do we mean by using !=
+print("Not Equal")
+
+# 3 - What is the operator precedence
+print("اولوية العمليات الحسابية")
+
+# 4 - Create a variable x with value of 10
+x = 10
+print(x)
+
+# 5 - Increase x value by 15 using assignment operators
+x = 20
+x += 15 # x = x + 15
+print(x)
+
+# 6 - Divide the x value by 5 using assignment operators
+x = 25
+x /= 5  # x = x / 5
+print(x)
+
+# 7 - Multiply the x value by 10 using assignment operator
+x = 20
+x *= 10   # x = x * 10
+print(x)
+
+# 8 - Get module of x on 3 using assignment operators
+x = 14
+x %= 3  # x = x % 3
+print(x)
+
+# 9 - Using python print the module of 11 to 4
+x = 18 % 4
+print(x)
+
+# 10 - Print the exponent of 2,3
+x = 2 ** 3
+print(x)
+
+# 11 - Divide 11 by 3 using python division
+x = 11 / 3
+print(x)
+
+# 12 - Can we divide 11 by 3 and get an integer number ?
+x = 11 / 3 # No => 11 / 3 = 3.66 (float)
+print(x)
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
